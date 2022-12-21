533f9d5bf6553860791d15d64adc372bb801b164
Fahim <fahimbd286623@gmail.com>
2022-12-21 19:16:10 +0600

    basic program done

---
+++ basic_dart_program/division.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  var a = 30, b = 2, result;
+  result = a / b;
+  print(result);
+}
---
+++ basic_dart_program/multiplication.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  var a = 4, b = 5, result;
+  result = a * b;
+  print(result);
+}
