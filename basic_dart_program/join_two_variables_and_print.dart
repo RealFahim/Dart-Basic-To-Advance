898785ec299f80bfebfcdfd4b7b9bf33d23885bc
Fahim <fahimbd286623@gmail.com>
2022-12-29 08:20:06 +0600

    test choltace

---
+++ basic_dart_program/join_two_variables_and_print.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var name1 = "Abdulla Al";
+
+  var name2 = "Ferdous Fahim";
+
+  print("My name is $name1 $name2");
+
+  //Another Way To print
+
+  print("My name is " + name1 + " And " + name2 + " . ");
+
+  var amount = 100.00;
+
+  print("Amar kase \$$amount ache");
+}
